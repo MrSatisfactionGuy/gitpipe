cf6dbb00a62d6a260b4eb21e3591bd909b531f3c
MrSatisfactionGuy <edd.wag18@gmail.com>
2024-03-06 19:28:26 +0100

    init

---
+++ gitpipe/Controllers/MinimumAPIController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace gitpipe.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+
+    public class MinimumAPIController
+    {
+        [HttpGet]
+        public bool Load()
+        {
+            return true;
+        }
+    }
+}
